717f26c2f0e7341793a99f655d0fc12207044416
ak2k2 <armaan.kapoor@cooper.edu>
2023-04-30 03:59:53 -0400

    add test for ends with hint

--- src/test/java/com/springboot/app/springbootfirstapp/HintGeneratorTest.java
+++ src/test/java/com/springboot/app/springbootfirstapp/HintGeneratorTest.java
@@ -16,4 +16,13 @@ public class HintGeneratorTest {
         assertEquals(5, generatedHint.length());
     }
 
+    @Test
+    public void testHint_endsWithLetter_yemen() throws Exception {
+        String question = "Name A Country in the World that ends with the letter n";
+        String generatedHint = HintGenerator.makeHint(question);
+        char lastLetterOfHint = generatedHint.charAt(generatedHint.length() - 1);
+        // make sure the last letter of the hint is 'n' (this is a deterministic aspect of the hint generator when the question type is "end")
+        assertEquals('n', lastLetterOfHint);
+    }
+
 }
